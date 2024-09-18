a1e84fd3ee024d813563634fc49c2f03c885dab0
Remy Matos <remy_matos@hotmail.com>
2024-09-18 08:30:03 -0400

    cambios

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Manuel Ernesto Garcia | Website';
-export const SITE_DESCRIPTION = 'Software Engineer from Cuba • JS • Minimalist • Creando soluciones y compartiendo lo que aprendo.';
+export const SITE_TITLE = 'Remy A. Matos | Website';
+export const SITE_DESCRIPTION = 'Cybersecurity Engineer from DR • CISO • Ethical Hacker • Red & Blue Teams';
